dff88273b785963fbeb2a9a380d6e31484397879
codeavenger1 <league@WTS-iMac-11.attlocal.net>
2017-09-28 16:57:28 -0700

    got load to work and add task
    fix remove task

--- src/intro_to_file_io/ToDo.java
+++ src/intro_to_file_io/ToDo.java
@@ -3,89 +3,88 @@ package intro_to_file_io;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.FileWriter;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class ToDo implements ActionListener{
-	JButton button;
-	JButton button2;
-	JButton button3;
-	JButton button4;
+public class ToDo implements ActionListener {
+	JButton add;
+	JButton remove;
+	JButton save;
+	JButton load;
 	String name;
 	String task;
+	String removet;
 	ToDo todo;
-	ArrayList <String> list = new ArrayList<String>();
-	Scanner scan = null;
-	PrintWriter pw = null;
+	ArrayList<String> list = new ArrayList<String>();
 	File file = new File("/Users/league/Desktop/ /src/intro_to_file_io/test.txt");
-	
-public  static void main(String[] args) {
-	ToDo todo = new ToDo();
-	todo.main2();
-	
+	FileWriter fw;
 
-	
-	
-}
-void main2() {
-	JFrame frame = new JFrame();
-	frame.setSize(500, 500);
-	frame.setVisible(true);
-	JPanel panel = new JPanel();
-	button = new JButton();
-	button2 = new JButton();
-	button3 = new JButton();
-	button4 = new JButton();
-	frame.add(panel);
-	panel.add(button);
-	panel.add(button2);
-	panel.add(button3);
-	panel.add(button4);
-	button.setText("add task");
-	button2.setText("remove task");
-	button3.setText("save");
-	button4.setText("load");
-	button.addActionListener(this);
-	button2.addActionListener(this);
-	button3.addActionListener(this);
-	button4.addActionListener(this);
-}
+	public static void main(String[] args) {
+		ToDo todo = new ToDo();
+		todo.main2();
 
-ToDo (){
-	try {
-		
-		scan = new Scanner(file);
-		pw = new PrintWriter(file);
-		
-		pw.close();
-		
-	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
 	}
-	while(scan.hasNextLine()) {
-		System.out.println(scan.nextLine());
-	}
-}
 
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	if (e.getSource() == button) {
-		//String name = JOptionPane.showInputDialog("What is your name?");
-		String task =JOptionPane.showInputDialog("Hi, my name is Siri, your personal assisstant. What is your task, " + name + "?" );
-		list.add(task);
-		
+	void main2() {
+		JFrame frame = new JFrame();
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+		JPanel panel = new JPanel();
+		add = new JButton();
+		remove = new JButton();
+		save = new JButton();
+		load = new JButton();
+		frame.add(panel);
+		panel.add(add);
+		panel.add(remove);
+		panel.add(save);
+		panel.add(load);
+		add.setText("add task");
+		remove.setText("remove task");
+		save.setText("save");
+		load.setText("load");
+		add.addActionListener(this);
+		remove.addActionListener(this);
+		save.addActionListener(this);
+		load.addActionListener(this);
 	}
-	if (e.getSource() == button4) {
-		pw.println(task);
+
+	ToDo() {
+
 	}
-}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == add) {
+			// String name = JOptionPane.showInputDialog("What is your name?");
+			task = JOptionPane.showInputDialog("What is your task?");
+			list.add(task + "\n");
+			System.out.println(task);
+		}
+		if (e.getSource() == load) {
+			try {
+				fw = new FileWriter("src/intro_to_file_io/ToDo", true);
+				for (int i = 0; i < list.size(); i++) {
+					fw.write(list.get(i));
+				}
+				fw.close();
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		}
+		if (e.getSource() == remove) {
+			// String name = JOptionPane.showInputDialog("What is your name?");
+			removet = JOptionPane.showInputDialog("What is the task you want to remove?");
+			if (list.contains(removet)) {
+				list.remove(removet.equals(" "));
+			}
+
+		}
+	}
 }
